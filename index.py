685d432792f670b73935fd54cf3fff108beb60cd
AliDz84 <140665302+AliDz84@users.noreply.github.com>
2023-07-27 03:47:38 +0100

    Add files via upload

---
+++ index.py
@@ -0,0 +1,64 @@
+import dash
+from dash import html
+from dash import dcc
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
+
+app=dash.Dash(__name__,use_pages=True,external_stylesheets=[dbc.themes.SUPERHERO])
+server = app.server
+sidebar=dbc.Nav(
+    [
+        dbc.NavLink(
+            [
+            html.Div(page["name"],className="lead"),
+            ],
+            href=page["path"],
+            active="exact",
+            )
+        for page in dash.page_registry.values()
+        ],
+    vertical=True,
+    pills=True,
+    className="bg-Blue",
+            )
+app.layout=dbc.Container([
+
+        dbc.Row(
+        [
+        dbc.Col([
+            html.Img(src="/assets/logo.jpg",className = 'left',style={'margin-top': '20px','margin-left':'20px'}),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+            html.Br(),
+                   
+            sidebar
+            ],xs=2,sm=2,md=2,lg=2,xl=2,xxl=2),
+        dbc.Col(
+                [
+            dbc.Row(
+                [           
+            html.Div("CONSTRUCTION OF SEPARATION AND DECARBONATION UNITS AT ALRAR EPC2 LOT 1",
+                        style={'frontsize':'50','textAlign':'center','color':'#ffffff','margin-top':'20px'}),
+            html.Div("- DASHBOARD",
+                        style={'frontsize':'50','bold':'True','textAlign':'center','color':'#ffffff'}),
+            html.Hr(),
+                    ]),
+            dbc.Row(
+                [
+                    dash.page_container
+                    ],)
+            ])
+
+            ]
+        ),
+
+    ],fluid=True)
+
+
+    
+
+if __name__ == '__main__':
+    app.run()
